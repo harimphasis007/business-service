2c4409cecf22930a4e26e452b1160f551c289ce6
neeleshparihar <pariharneelesh@gmail.com>
2020-01-04 14:46:54 -0500

    resolved conflict

--- src/main/java/com/business/service/web/rest/controller/CreditCheckController.java
+++ src/main/java/com/business/service/web/rest/controller/CreditCheckController.java
@@ -4,13 +4,11 @@ import com.business.service.service.CreditCheckService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/credit")
+@CrossOrigin(origins = "*")
 public class CreditCheckController {
     // Logger instance
     private static final Logger logger = LoggerFactory.getLogger(CreditCheckController.class);
